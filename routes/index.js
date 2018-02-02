84144cbdd6d4b84433641b354abda26b3d90b4ee
KANG BIN KIM <kimkb2011@gmail.com>
2018-02-02 01:49:38 -0500

    send email without smtp using nodemailer.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
+var nodemailer = require('nodemailer');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+var transporter = nodemailer.createTransport({
+    sendmail:true,
+});
+
+
+
+
+
+
+
+const mailOptions = {
+    from: 'GeorgePBurdell@gatech.edu', // sender address
+    to: 'info@fraudmarc.com', // list of receivers
+    subject: 'Hiring bonus for Jeff Kang Bin Kim', // Subject line
+    html: '<p>This is not a spam. I am serious.</p>'// plain text body
+};
+
+transporter.sendMail(mailOptions, function (err, info) {
+    if(err)
+        console.log(err);
+    else
+        console.log(info);
 });
 
 module.exports = router;
